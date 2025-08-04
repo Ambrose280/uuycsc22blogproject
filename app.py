ad9703b9f5909b759080b8ee27e7965fea1463c3
Ifiok Ambrose <ifiokambrose@gmail.com>
2025-08-04 13:45:49 +0100

    setup basic endpoints with templates

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from databaseconnector import db
 from flask_migrate import Migrate
 from flask import jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -15,10 +17,38 @@ with app.app_context():
 # Initialize Flask Migrate
 migrate = Migrate(app, db)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            return redirect(url_for('home'))
+    return render_template('login.html')
 
-@app.route('/')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
+@app.route('/pageinfo', methods=['GET'])
+def pageinfo():
+    return render_template('pageinfo.html')
+
+@app.route('/createpost', methods=['GET', 'POST'])
+def createpost():
+    return render_template('createpost.html')
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/', methods=['GET'])
 def index():
-    return jsonify({"message": "Hello, world!"})
+    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
