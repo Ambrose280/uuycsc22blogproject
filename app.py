db93b119f406ac60a3ba9702c3e1ba6b069779dc
Ifiok Ambrose <ifiokambrose@gmail.com>
2025-08-05 13:47:24 +0100

    ready for vercel deployment

--- app.py
+++ app.py
@@ -352,8 +352,8 @@ def logout():
     return redirect(url_for('index'))
 
 
-if __name__ == '__main__':
-    if not os.path.exists('blog.db'):
-        with app.app_context():
-            init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     if not os.path.exists('blog.db'):
+#         with app.app_context():
+#             init_db()
+#     app.run(debug=True, host='0.0.0.0', port=5000)
